e121a9ddc2f25e5964be4feeb2e05257d2185193
David Waern <david@evidentiaetechnologies.com>
2017-05-12 13:40:52 +0000

    nix/nix-builds: Allow passing arguments to builds

--- nix/nix-builds.nix
+++ nix/nix-builds.nix
@@ -20,8 +20,25 @@ let
         type = types.bool;
         default = true;
       };
+      arguments = mkOption {
+        type = types.attrs;
+        default = {};
+        description = ''
+          Pass arguments to the build definition. If this is used,
+          the build definition should be a function taking the
+          passed arguments as parameters. Note that the parameters
+          are *not* passed if the build is run by Hydra, only when
+          the build is run in the CLI. Because of this, all parameters
+          of the build definition function should have default values.
+          Also note that currently only boolean arguments are supported.
+        '';
+      };
     };
   };
+
+  passArgs = args: concatStringsSep " " (mapAttrsToList (name: value:
+    "--arg ${name} ${if value then "true" else "false"}") args
+  );
 
 in {
   imports = [ ./cli.nix ];
@@ -36,6 +53,7 @@ in {
       #!${stdenv.shell}
       exec ${nix}/bin/nix-build ${if build.fallback then "--fallback" else ""} \
         '<${build.nixPath.top}/${build.nixPath.sub}>' \
+        ${passArgs build.arguments} \
         "$@"
     '';
   }) config.nixBuilds;

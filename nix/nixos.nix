eb74b3a6cbe93bf3da1a7011584629699687604c
David Waern <david.waern@gmail.com>
2018-01-12 14:28:31 +0100

    Remove nixos.pathOverride

--- nix/nixos.nix
+++ nix/nixos.nix
@@ -27,25 +27,6 @@ with builtins;
           `nixos/modules-list.nix` in the nixpkgs repo).
         '';
       };
-      pathOverride = mkOption {
-        type = with types; nullOr unspecified;
-        default = null;
-        example = literalExample ''
-          paths: paths // {
-            nixpkgs = paths.nixpkgs-1412;
-            nixpkgs-master = pkgs.fetchgit {
-              url = "https://github.com/NixOS/nixpkgs.git";
-              rev = "8e6acbe2011be0939eafd4772fcc837f81aaf4fb";
-              sha256 = "1hhj9sin6984214vias8p520whmm1fil0a7xvhnapx05xfzaz9vh";
-            };
-          }
-        '';
-        description = ''
-          This option lets you override the Nix paths used for evaluating the
-          NixOS build. It should be set to a function that takes an attribute
-          set of prefix/path pairs and returns a new attribute set.
-        '';
-      };
       out = mkOption {
         type = types.unspecified;
         description = ''
@@ -57,39 +38,18 @@ with builtins;
 
   config = {
     nixos.out =
-      if config.nixos.pathOverride == null then
-        let
-          args = {
-            system = builtins.currentSystem;
-            modules = [ { inherit (config.nixos) imports; } ];
-          } // optionalAttrs (config.nixos.baseImports != null) {
-            baseModules = config.nixos.baseImports;
-          };
-
-          eval = import "${toString pkgs.path}/nixos/lib/eval-config.nix" args;
-        in {
-          inherit (eval) config options;
-          system = eval.config.system.build.toplevel;
-        }
-      else (
-        let overrides = {
-          __nixPath = sort (p1: p2: p1.prefix > p2.prefix) (
-            mapAttrsToList (prefix: path: { inherit prefix path; }) (
-              config.nixos.pathOverride (
-                listToAttrs (map (x: nameValuePair x.prefix x.path) __nixPath)
-              )
-            )
-          );
-          import = fn: scopedImport overrides fn;
-          scopedImport = attrs: fn: scopedImport (overrides // attrs) fn;
-          builtins = builtins // overrides;
+      let
+        args = {
+          system = builtins.currentSystem;
+          modules = [ { inherit (config.nixos) imports; } ];
+        } // optionalAttrs (config.nixos.baseImports != null) {
+          baseModules = config.nixos.baseImports;
         };
-        in let inherit (overrides) __nixPath;
-        in scopedImport overrides <nixpkgs/nixos> {
-          configuration = {
-            inherit (config.nixos) imports;
-          };
-        }
-      );
+
+        eval = import "${toString pkgs.path}/nixos/lib/eval-config.nix" args;
+      in {
+        inherit (eval) config options;
+        system = eval.config.system.build.toplevel;
+      };
   };
 }

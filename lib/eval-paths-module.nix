fecb14907cd8a75a183a0e02d1ae335ee0a21ade
Rickard Nilsson <rickynils@gmail.com>
2018-10-17 22:25:01 +0200

    Move infranix eval module to lib/

--- lib/eval-paths-module.nix
+++ lib/eval-paths-module.nix
@@ -1,29 +1,32 @@
 { paths
 , configuration
-, lib ? import "${paths.nixpkgs}/lib"
 }:
 
 with builtins;
 
 let
 
+  pathAttrs = if isAttrs paths then paths else import paths;
+
+  lib = import "${pathAttrs.nixpkgs}/lib";
+
   pkgsModule = { lib, config, ... }: {
     nixpkgs.system = lib.mkDefault currentSystem;
     nixpkgs.pkgs = lib.mkDefault (
-      import paths.nixpkgs {
+      import pathAttrs.nixpkgs {
         inherit (config.nixpkgs) config overlays localSystem crossSystem;
       }
     );
   };
 
   eval = lib.evalModules {
-    specialArgs.paths = paths;
+    specialArgs.paths = pathAttrs;
     modules = [
       configuration
       pkgsModule
       # We reuse this module from NixOS to be able to set nixpkgs modularily
-      "${paths.nixpkgs}/nixos/modules/misc/nixpkgs.nix"
+      "${pathAttrs.nixpkgs}/nixos/modules/misc/nixpkgs.nix"
     ];
   };
-      
+
 in eval
--- nixpkgs/infranix/default.nix
+++ nixpkgs/infranix/default.nix
@@ -3,8 +3,9 @@
 runCommand "infranix" {
   buildInputs = [ makeWrapper ];
   src = ./infranix.sh;
+  EVAL = ../../lib/eval-paths-module.nix;
 } ''
   mkdir -p $out/bin
   ln -s "$src" $out/bin/infranix
-  wrapProgram $out/bin/infranix --set EVAL "${./eval.nix}"
+  wrapProgram $out/bin/infranix --set EVAL "$EVAL"
 ''

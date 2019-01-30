f0c2d60516948d91ecf5c1db4e9321d6263e95c8
Rickard Nilsson <rickynils@gmail.com>
2019-01-30 23:37:29 +0100

    resources/nixos-hosts: Add cli command for launching shells

--- nix/resources/nixos-hosts.nix
+++ nix/resources/nixos-hosts.nix
@@ -1,5 +1,6 @@
 { paths, config, lib, pkgs, ... }:
 
+with pkgs;
 with lib;
 
 let
@@ -67,7 +68,9 @@ let
     config = {
       inherit name;
       nixos = {
-        imports = cfg.commonNixosImports;
+        imports = cfg.commonNixosImports ++ singleton {
+          networking.hostName = mkDefault name;
+        };
         baseImports = cfg.commonBaseImports;
         out =
           let
@@ -89,6 +92,10 @@ let
     };
 
 in {
+
+  imports = [
+    ../cli.nix
+  ];
 
   options = {
 
@@ -116,6 +123,75 @@ in {
       };
 
     };
+
+  };
+
+  config = {
+
+    cli.commands.server.subCommands.info.binary = mkDefault (writeScript "server-info" ''
+      #!${stdenv.shell}
+
+      if [ "$#" -lt 2 ]; then
+        exit 1
+      fi
+
+      function echoOrExec() {
+        if [[ "$1" =~ ^/.* ]]; then
+          "$1"
+        else
+          echo "$1"
+        fi
+      }
+
+      case "$1" in
+        ${concatMapStrings (host: ''
+          ${host.name}):
+            case "$2" in
+              ssh_address) echoOrExec "${host.ssh.address}" ;;
+              ssh_args) echo "${concatStringsSep " " host.ssh.extraArgs}" ;;
+              nixstore_ssh_address) echoOrExec "${host.nixos.store.ssh.address}" ;;
+              nixstore_ssh_args) echo "${concatStringsSep " " host.nixos.store.ssh.extraArgs}" ;;
+              *) exit 1 ;;
+            esac
+            ;;
+        '') (attrValues cfg.hosts)}
+        *)
+        echo >&2 "Server $1 does not exist"
+        exit 1
+        ;;
+      esac
+    '');
+
+    cli.commands.server.subCommands.shell.binary = writeScript "server-shell" ''
+      #!${stdenv.shell}
+      set -eu
+      set -o pipefail
+
+      server_info=${config.cli.commands.server.subCommands.info.binary}
+
+      if [ "$#" -lt 1 ]; then
+        echo >&2 "No server specified"
+        exit 1
+      fi
+
+      ssh_address="$($server_info "$1" ssh_address)"
+      ssh_args="$($server_info "$1" ssh_args)"
+      nixstore_ssh_address="$($server_info "$1" nixstore_ssh_address)"
+      nixstore_ssh_args="$($server_info "$1" nixstore_ssh_args)"
+
+      shift 1
+
+      if [[ "$nixstore_ssh_address" != localhost ]] && [ "$#" -gt 0 ] && [[ "$1" == "${builtins.storeDir}"* ]]; then
+        NIX_SSHOPTS="-lroot $nixstore_ssh_args" \
+          nix copy -s --to "ssh://$nixstore_ssh_address" "$1"
+      fi
+
+      exec ssh -lroot $ssh_args "$ssh_address" "$@"
+    '';
+
+    cli.commands.server.subCommands.shell.completions = singleton (
+      concatStringsSep "|" (attrNames cfg.hosts)
+    );
 
   };
 

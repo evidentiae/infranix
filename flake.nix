0a05fc87e7915ca4ab90f61b444cf4bc7a0504e2
Rickard Nilsson <rickynils@gmail.com>
2020-09-03 15:11:56 +0200

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -3,33 +3,24 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/20.03";
 
-  outputs = {self, nixpkgs}:
+  outputs = {self, nixpkgs}: {
 
-    let
+    lib.evalModulesWithInputs = {inputs ? {}, ...}@attrs:
+      nixpkgs.lib.evalModules ((builtins.removeAttrs attrs ["inputs"]) // {
+        specialArgs = (attrs.specialArgs or {}) // {
+          inherit inputs;
+          paths = inputs; # for backwards compatibility
+        };
+      });
 
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
+    lib.mkShell = system: {modules, ...}@attrs:
+      (self.lib.evalModulesWithInputs (attrs // {
+        modules = modules ++ [
+          (nixpkgs + "/nixos/modules/misc/nixpkgs.nix")
+          { nixpkgs.system = system; }
+          ./nix/cli.nix
+        ];
+      })).config.cli.build.shell;
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-
-    in {
-
-      lib.evalModulesWithInputs = {inputs ? {}, ...}@attrs:
-        nixpkgs.lib.evalModules ((builtins.removeAttrs attrs ["inputs"]) // {
-          specialArgs = (attrs.specialArgs or {}) // {
-            inherit inputs;
-            paths = inputs; # for backwards compatibility
-          };
-        });
-
-      lib.mkShells = {modules, ...}@attrs: forAllSystems (system:
-        (self.lib.evalModulesWithInputs (attrs // {
-          modules = modules ++ [
-            (nixpkgs + "/nixos/modules/misc/nixpkgs.nix")
-            { nixpkgs.system = system; }
-            ./nix/cli.nix
-          ];
-        })).config.cli.build.shell
-      );
-
-    };
+  };
 }

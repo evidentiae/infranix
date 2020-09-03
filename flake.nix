1cb436c2fd7349a4c10288a2787cc797a6d0a2d5
Rickard Nilsson <rickynils@gmail.com>
2020-09-03 13:08:08 +0200

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,35 @@
 {
-
   description = "infranix";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/20.03";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {self, nixpkgs}:
 
-  outputs = { self, nixpkgs, flake-utils }: {
+    let
 
-    lib.mkShell = inputs: module:
-      flake-utils.lib.eachDefaultSystem (system: {
-        defaultApp = (nixpkgs.lib.evalModules {
-          specialArgs.inputs = inputs;
-          specialArgs.paths = inputs; # for backwards compatibility
-          modules = [
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+    in {
+
+      lib.evalModulesWithInputs = {inputs ? {}, ...}@attrs:
+        nixpkgs.lib.evalModules ((builtins.removeAttrs attrs ["inputs"]) // {
+          specialArgs = (attrs.specialArgs or {}) // {
+            inherit inputs;
+            paths = inputs; # for backwards compatibility
+          };
+        });
+
+      lib.mkShells = {modules, ...}@attrs: forAllSystems (system:
+        (self.lib.evalModulesWithInputs (attrs // {
+          modules = modules ++ [
             (nixpkgs + "/nixos/modules/misc/nixpkgs.nix")
             { nixpkgs.system = system; }
             ./nix/cli.nix
-            module
           ];
-        }).config.cli.build.shell;
-      });
+        })).config.cli.build.shell
+      );
 
-  };
-
+    };
 }
